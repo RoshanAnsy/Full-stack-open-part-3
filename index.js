b5ac661e86b8a71a715d500e56b878c6a8c3cced
Nick van den Dungen <nvan-den@student.hive.fi>
2023-10-02 20:17:13 +0300

    setup

--- index.js
+++ index.js
@@ -3,42 +3,54 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (request, response) => 
+const people = 
+[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (req, res) => 
 {
-  response.send('<h1>Hello FUCK!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => 
+app.get('/api/persons', (req, res) => 
 {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  res.json(persons)
 })
 
-app.delete('/api/notes/:id', (request, response) => 
+const generateId = () => 
 {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/persons', (request, response) => 
+{
   const body = request.body
 
-  if (!body.content) {
+  if (!body.content) 
+  {
     return response.status(400).json({ 
       error: 'content missing' 
     })
@@ -47,15 +59,40 @@ app.post('/api/notes', (request, response) => {
   const note = {
     content: body.content,
     important: body.important || false,
+    date: new Date(),
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  persons = persons.concat(note)
 
   response.json(note)
 })
 
+app.get('/api/persons/:id', (request, response) => 
+{
+  const id = Number(request.params.id)
+  const note = persons.find(note => note.id === id)
+
+  if (note) 
+  {
+    response.json(note)
+  } else 
+  {
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/persons/:id', (request, response) => 
+{
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
-app.listen(PORT, () => {
+app.listen(PORT, () => 
+{
   console.log(`Server running on port ${PORT}`)
 })
